051324ee8a8ea84ead56084a4abc2f93d0f541ee
Jack Hessel <jmhessel@gmail.com>
2016-10-05 12:51:20 -0400

    added compression

--- download_ids.py
+++ download_ids.py
@@ -3,8 +3,9 @@ import os, subprocess
 
 train_ids_dir = 'train_ids_dir'
 outf = 'training_ids.txt'
+outf_c = 'training_ids.zip'
 
-if os.path.exists(outf):
+if os.path.exists(outf_c):
     print("Are you sure you want to run this script? The ids are already scraped.")
     print("If so, please delete {} and re-run this.".format(outf))
     quit()
@@ -23,7 +24,6 @@ def get_ids_from_csv(csv_in, out_dir):
         lines = f.readlines()[1:]
         for i, line in enumerate(lines):
             if i % 100 == 0: print("{}/{}".format(i, len(lines)))
-            break
             cid = line.split(",")[1].split("/")[-1]
             if os.path.exists('{}/{}'.format(out_dir, cid)): continue
             subprocess.call('wget --quiet {} -O {}/{}'.format(base.format(cid),
@@ -32,7 +32,7 @@ def get_ids_from_csv(csv_in, out_dir):
             
 get_ids_from_csv('train-labels-histogram.csv', train_ids_dir)
 
-fs = os.listdir(train_ids_dir)
+fs = [f for f in os.listdir(train_ids_dir) if f != 'README.md']
 all_ids = []
 
 for f in fs:
@@ -44,5 +44,9 @@ for f in fs:
         ids = x.split(';')
         all_ids.extend(ids)
         
-with open(outf, 'w') as outf:
-    outf.write('\n'.join(all_ids) + '\n')
+with open(outf, 'w') as f:
+    f.write('\n'.join(all_ids) + '\n')
+
+print(outf, outf_c)
+print('zip {} {}'.format(outf, outf_c))
+subprocess.call('zip {} {}'.format(outf_c, outf), shell = True)
